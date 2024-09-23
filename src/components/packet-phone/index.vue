85eb1e6993429ae0de55c8270e30c1bc7d569fc5
zhangkai <Kai.Zhang25@geely.com>
2024-09-23 22:40:10 +0800

    feat：添加打赏功能

---
+++ src/components/packet-phone/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="packet-phone">
+    <el-tooltip effect="light" content="打赏" placement="top">
+      <div class="items-content packet">
+        <img src="../../assets/images/packet.png" @click="visible = true" />
+      </div>
+    </el-tooltip>
+    <el-tooltip effect="light" content="联系我们" placement="top">
+      <div class="items-content phone">
+        <img src="../../assets/images/phone.png" @click="visible = true" />
+      </div>
+    </el-tooltip>
+    <el-dialog
+      top="25%"
+      width="40%"
+      v-model="visible"
+      :close-on-click-modal="false"
+    >
+    </el-dialog>
+  </div>
+</template>
+
+<script setup name="PacketPhone">
+import { ref } from "vue";
+
+const visible = ref(false);
+</script>
+<style lang="scss" scoped>
+.packet-phone {
+  right: 8%;
+  bottom: 10%;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+  .items-content {
+    display: flex;
+    align-items: center;
+    img {
+      width: 32px;
+      height: 32px;
+      cursor: pointer;
+      margin: 0px 6px;
+    }
+  }
+}
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="page-home">
     <div class="page-home-form">
-      <el-form :model="form" label-width="auto">
-        <el-form-item label="GitHub地址：">
+      <el-form ref="formEl" :model="form" :rules="rules" label-width="auto">
+        <el-form-item label="GitHub地址：" prop="gitUrls">
           <el-input v-model="form.gitUrls" />
+          <div class="desc-text">仅支持 HarmonyOS Next 应用</div>
         </el-form-item>
-        <el-form-item label="应用名称：">
+        <el-form-item label="应用名称：" prop="appName">
           <el-input v-model="form.appName" />
         </el-form-item>
-        <el-form-item label="应用包名：">
+        <el-form-item label="应用包名：" prop="packageName">
           <el-input v-model="form.packageName" />
         </el-form-item>
         <div class="btns-content">
-          <el-button type="primary" @click="onLogin">开始构建</el-button>
+          <el-button type="primary" @click="submitForm">开始构建</el-button>
         </div>
       </el-form>
     </div>
@@ -30,7 +31,7 @@
         >
           <template #extra>
             <el-form-item label="下载地址：">
-              <el-input v-model="status.dowloadUrl">
+              <el-input disabled v-model="status.dowloadUrl">
                 <template #append>
                   <el-tooltip effect="light" placement="top" content="复制">
                     <el-button :icon="CopyDocument" />
@@ -42,14 +43,26 @@
         </el-result>
       </el-form>
     </div>
+    <PacketPhone />
   </div>
 </template>
 
 <script setup name="HomePage">
 import { ref, reactive } from "vue";
 import { CopyDocument } from "@element-plus/icons-vue";
+import PacketPhone from "@/components/packet-phone/index.vue";
 import PublishSteps from "@/components/publish-steps/index.vue";
 
+const formEl = ref(null);
+const rules = reactive({
+  appName: [{ required: true, message: "应用名称不允许为空", trigger: "blur" }],
+  gitUrls: [
+    { required: true, message: "GitHub地址不允许为空", trigger: "blur" },
+  ],
+  packageName: [
+    { required: true, message: "应用包名不允许为空", trigger: "blur" },
+  ],
+});
 const form = reactive({ appName: "", gitUrls: "", packageName: "" });
 const status = reactive({
   icon: "success",
@@ -57,9 +70,16 @@ const status = reactive({
   dowloadUrl: "www.baidu.com",
 });
 
-const onLogin = () => {
-  console.log("111");
-  window.Api.toLogin();
+const submitForm = async () => {
+  if (!formEl.value) return;
+  await formEl.value.validate((valid, fields) => {
+    if (valid) {
+      console.log("submit!");
+      // window.Api.toLogin();
+    } else {
+      console.log("error submit!", fields);
+    }
+  });
 };
 </script>
 
@@ -79,6 +99,17 @@ const onLogin = () => {
     :deep(.el-form),
     :deep(.el-result__extra) {
       width: 100%;
+      .desc-text {
+        width: 100%;
+        text-align: right;
+        font-size: 12px;
+        left: 0;
+        top: 100%;
+        line-height: 1;
+        padding-top: 2px;
+        position: absolute;
+        color: var(--el-color-danger);
+      }
       .btns-content {
         display: flex;
         align-items: center;
