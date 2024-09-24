744e5b8461eff3a9d582a59f5d607d6bb06b8320
李快 <likuai@fiber-doctor.com>
2024-09-24 18:22:49 +0800

    抓去部分接口

--- core/agcService.js
+++ core/agcService.js
@@ -39,9 +39,43 @@ class AgcService{
     }   
 }
 
-function getCookie(){
 
-}
+/**
+ * 
+ * {"inputParameters":{"configNames":["app.distribution.device.config","app.package.name.sensitive.words","app.reserved.package.name.list","app.category.sensitive.words"],"checkedGroups":{"checkedGroups":[{"id":"noApk.whiteList","userType":1},{"id":"app.pc.user.allow.list","userType":1},{"id":"app.pc.internal.vmp.team.list","userType":1},{"id":"sysUrlCfg.whitelistUsers","userType":1},{"id":"app.cloudpc.user.whitelist","userType":1},{"id":"smart.cockpit.user.allow.list"},{"id":"harmony.app.support.2in1.allow.list"},{"id":"authorized.app.allow.operate.list"}]},"langType":"zh_CN","appDevelopStatus":1}}
+ * https://agc-drcn.developer.huawei.com/agc/edge/apios/invokeService/AGCDistOrchestration/GetApplistInitInfo
+ * 
+ * get
+ * https://agc-drcn.developer.huawei.com/agc/edge/sfs/group-management/v1/group/check?id=harmony.reserved.package.name.allow.list&item=x.x.dddd
+ * 
+ * {"packageName":"x.x.dddd"}
+ * https://agc-drcn.developer.huawei.com/agc/edge/amis/app-manage/v1/app/check/package-name
+ * 
+ * {"fromPage":1,"pageSize":100,"projectIds":[],"projectName":"","queryFlag":1}
+ * https://agc-drcn.developer.huawei.com/agc/edge/cpms/project-management-service/v1/projectList
+ * 
+ * 
+ * {"name":"xiaobai","createDevProjectFlag":0,"createTenantProjectFlag":0,"createAllianceProjectFlag":1}
+ * https://agc-drcn.developer.huawei.com/agc/edge/cpms/project-management-service/v1/projects
+ * 
+ * 
+ * {"appName":"12412","packageName":"x.x.dddd","defaultLang":"zh-CN","parentType":13,"createCountry":"CN","projectId":"388421841222508849","packageType":7,"deviceTypes":[{"deviceType":4}],"appDevelopStatus":0,"installationFree":0,"createChannel":0}
+ * https://agc-drcn.developer.huawei.com/agc/edge/amis/app-manage/v1/app
+ * 
+ * GET
+ * https://agc-drcn.developer.huawei.com/agc/edge/aps/api-permission-service/v1/client-list?type=1&page=1&limit=20&productId=
+ * 
+ * {"name":"1111","type":1,"roles":["com.huawei.connect.role.appmanager","com.huawei.connect.role.operator","com.huawei.connect.role.developer","com.huawei.connect.role.financial","com.huawei.connect.role.support"],"products":[]}
+ * https://agc-drcn.developer.huawei.com/agc/edge/aps/api-permission-service/v1/client
+ *  */ 
+
+
+
+
+
+
+
+
 module.exports = {
     AgcService
 }
