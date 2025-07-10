98801cff26ccf9e7acc392a3a5eae5338829f5e2
李快 <likuai@fiber-doctor.com>
2025-07-10 19:12:30 +0800

    fix docker build

--- flutter/hap_installer/jdk/build.sh
+++ flutter/hap_installer/jdk/build.sh
@@ -1,37 +1,79 @@
 
 #!/bin/bash
 
-mkdir -p sysroot/aarch64-unknown-linux-musl/lib
-cp $OHOS_SDK_HOME/native/sysroot/usr/lib/aarch64-linux-ohos/crt1.o sysroot/aarch64-unknown-linux-musl/lib/crt1.o
-cp $OHOS_SDK_HOME/native/sysroot/usr/lib/aarch64-linux-ohos/crti.o sysroot/aarch64-unknown-linux-musl/lib/crti.o
-cp $OHOS_SDK_HOME/native/sysroot/usr/lib/aarch64-linux-ohos/crtn.o sysroot/aarch64-unknown-linux-musl/lib/crtn.o
-cp $OHOS_SDK_HOME/native/llvm/lib/clang/15.0.4/lib/aarch64-linux-ohos/clang_rt.crtbegin.o sysroot/aarch64-unknown-linux-musl/lib/crtbegin.o
-cp $OHOS_SDK_HOME/native/llvm/lib/clang/15.0.4/lib/aarch64-linux-ohos/clang_rt.crtend.o sysroot/aarch64-unknown-linux-musl/lib/crtend.o
-cp $OHOS_SDK_HOME/native/llvm/lib/clang/15.0.4/lib/aarch64-linux-ohos/libclang_rt.builtins.a sysroot/aarch64-unknown-linux-musl/lib/libgcc.a
-cp $OHOS_SDK_HOME/native/llvm/lib/clang/15.0.4/lib/aarch64-linux-ohos/libclang_rt.builtins.a sysroot/aarch64-unknown-linux-musl/lib/libgcc_s.a
+# /opt/ohos-sdk/linux/native/llvm/bin
+# cp $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-ohos-clang $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-musl-clang
+# cp $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-ohos-clang++ $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-musl-clang++
+# cp $OHOS_SDK_HOME/native/llvm/bin/lld $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-musl-ld
+# cp $OHOS_SDK_HOME/native/llvm/bin/llvm-nm $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-musl-nm
+# cp $OHOS_SDK_HOME/native/llvm/bin/llvm-strip $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-musl-strip
+# cp $OHOS_SDK_HOME/native/llvm/bin/llvm-ar $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-musl-ar
+# cp $OHOS_SDK_HOME/native/llvm/bin/llvm-objcopy $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-musl-objcopy
+# cp $OHOS_SDK_HOME/native/llvm/bin/llvm-objdump $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-musl-objdump
+# cp $OHOS_SDK_HOME/native/llvm/bin/llvm-cxxfilt $OHOS_SDK_HOME/native/llvm/bin/aarch64-unknown-linux-musl-c++filt
+
+# mkdir -p sysroot/aarch64-unknown-linux-musl/lib
+# cp $OHOS_SDK_HOME/native/sysroot/usr/lib/aarch64-linux-ohos/crt1.o sysroot/aarch64-unknown-linux-musl/lib/crt1.o
+# cp $OHOS_SDK_HOME/native/sysroot/usr/lib/aarch64-linux-ohos/crti.o sysroot/aarch64-unknown-linux-musl/lib/crti.o
+# cp $OHOS_SDK_HOME/native/sysroot/usr/lib/aarch64-linux-ohos/crtn.o sysroot/aarch64-unknown-linux-musl/lib/crtn.o
+# cp $OHOS_SDK_HOME/native/llvm/lib/clang/15.0.4/lib/aarch64-linux-ohos/clang_rt.crtbegin.o sysroot/aarch64-unknown-linux-musl/lib/crtbegin.o
+# cp $OHOS_SDK_HOME/native/llvm/lib/clang/15.0.4/lib/aarch64-linux-ohos/clang_rt.crtend.o sysroot/aarch64-unknown-linux-musl/lib/crtend.o
+# cp $OHOS_SDK_HOME/native/llvm/lib/clang/15.0.4/lib/aarch64-linux-ohos/libclang_rt.builtins.a sysroot/aarch64-unknown-linux-musl/lib/libgcc.a
+# cp $OHOS_SDK_HOME/native/llvm/lib/clang/15.0.4/lib/aarch64-linux-ohos/libclang_rt.builtins.a sysroot/aarch64-unknown-linux-musl/lib/libgcc_s.a
 
 
 export NDK_HOME=$OHOS_SDK_HOME/native
 export TOOLCHAIN=$NDK_HOME/llvm
 export SYSROOT=$TOOLCHAIN/sysroot
 export PATH=$TOOLCHAIN/bin:$PATH
+
+
 export CC="$TOOLCHAIN/bin/clang"
 export CXX="$TOOLCHAIN/bin/clang++"
-export AR=llvm-ar
-export AS=llvm-as
-export LD=ld.lld
-export STRIP=llvm-strip
+export NM="$TOOLCHAIN/bin/llvm-nm"
+export LD="$TOOLCHAIN/bin/clang"
 
 
+# export CC=aarch64-linux-gnu-gcc
+# export CXX=aarch64-linux-gnu-g++
+# export AR=aarch64-linux-gnu-ar
+# export LD=aarch64-linux-gnu-ld
+# export RANLIB=aarch64-linux-gnu-ranlib
+# export STRIP=aarch64-linux-gnu-strip
+# export PKG_CONFIG=aarch64-linux-gnu-pkg-config
+# export PKG_CONFIG_PATH=/usr/aarch64-linux-gnu/lib/pkgconfig
 
-pushd jdk-jdk-17-10
+pushd jdk-17.0.16
+# bash configure \
+# --openjdk-target=aarch64-linux-gnu \
+# --with-toolchain-type=gcc \
+# --enable-headless-only \
+# --with-jvm-variants=server \
+# --disable-warnings-as-errors \
+# --with-freetype=bundled \
+# LD=${LD}
+# BUILD_LD=${LD} 
 bash configure \
 --openjdk-target=aarch64-unknown-linux-musl \
---with-boot-jdk=/usr/lib/jvm/java-17-openjdk-arm64/ \
---with-toolchain-type=gcc \
+--with-toolchain-type=clang \
 --enable-headless-only \
+--with-sysroot=${SYSROOT} \
+--with-toolchain-path=${TOOLCHAIN} \
 --with-jvm-variants=server \
---disable-warnings-as-errors
+--disable-warnings-as-errors \
+--with-freetype=bundled \
+--with-cups-include=/usr/include \
+--with-fontconfig-include=/usr/include/ \
+--with-alsa-include=/usr/include/ \
+--with-extra-cflags="-Wno-error --target=aarch64-linux-ohos --sysroot=$NDK_HOME/sysroot"  \
+--with-extra-ldflags="-Wno-error -extld=$LD --target=aarch64-linux-ohos --sysroot=$NDK_HOME/sysroot"  \
+--with-extra-cxxflags="-Wno-error --target=aarch64-linux-ohos --sysroot=$NDK_HOME/sysroot" \
+BUILD_CC=${CC} \
+BUILD_CXX=${CXX} \
+BUILD_NM=${NM} \
+LD=${LD} \
+BUILD_LD=${LD}
+
 
 
 popd
