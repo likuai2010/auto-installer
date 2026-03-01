1af01cd66bf195a9e68a3e77b0bc9c10cabf1d82
SummerKaze <h1007854589@outlook.com>
2026-03-01 18:27:54 +0800

    chore: 更新依赖配置和平台配置
    - 添加 flutter_svg 依赖
    - 移除 syncfusion_flutter_pdfviewer 依赖
    - 更新 macOS/Windows/HarmonyOS 平台配置

--- flutter/hap_installer/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/hap_installer/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,21 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import desktop_drop
-import device_info_plus
 import path_provider_foundation
 import pdf_render
 import shared_preferences_foundation
-import syncfusion_pdfviewer_macos
 import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SwiftPdfRenderPlugin.register(with: registry.registrar(forPlugin: "SwiftPdfRenderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
--- flutter/hap_installer/windows/flutter/generated_plugins.cmake
+++ flutter/hap_installer/windows/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   desktop_drop
-  syncfusion_pdfviewer_windows
   url_launcher_windows
 )
 
