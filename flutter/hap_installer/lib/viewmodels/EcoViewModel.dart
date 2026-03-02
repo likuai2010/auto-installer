db52a1acdbc25d8166eec22d28aa94fc2b649d4f
xiaobai <xiaobai@xiaobai.com>
2026-03-02 21:04:28 +0800

    修复App格式特殊权限问题

--- flutter/hap_installer/lib/viewmodels/EcoViewModel.dart
+++ flutter/hap_installer/lib/viewmodels/EcoViewModel.dart
@@ -513,7 +513,7 @@ class EcoViewModel extends ChangeNotifier {
       pathList = [hapPath];
     }
     final err = await cmd.unzip_Hap(
-      pathList.first,
+      pathList.last,
       "module.json",
       path.join(debugPath, "module.json"),
     );
